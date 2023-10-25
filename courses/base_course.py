8fa77ca5c1ac43baa53a0ad3639f536b275086fe
Jingchao Zhong <92573736+perryzjc@users.noreply.github.com>
2023-10-24 19:42:54 -0700

    Refract code for generlalzaition

---
+++ courses/base_course.py
@@ -0,0 +1,28 @@
+import requests
+import sys
+
+class BaseCourse:
+    def __init__(self, url):
+        self.url = url
+
+    def get_html(self):
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()
+            return response.text
+        except requests.RequestException as e:
+            print(f"Request failed: {e}")
+            sys.exit(1)
+
+    def parse_html(self, html):
+        raise NotImplementedError("parse_html method must be implemented by the subclass")
+
+    def check_availability(self):
+        html = self.get_html()
+        available, message = self.parse_html(html)
+        if available:
+            print(f"{self.__class__.__name__} is available! {message}")
+            return f"{self.__class__.__name__} is available! {message}"
+        else:
+            print(f"{self.__class__.__name__} is not available. {message}")
+            return None
---
+++ main.py
@@ -0,0 +1,43 @@
+import sys
+import importlib
+
+COURSE_TO_DETECT = [
+    'CS168',
+]
+
+def check_course_availability(course_name):
+    try:
+        course_module = importlib.import_module(f"courses.{course_name.lower()}")
+    except ModuleNotFoundError:
+        print(f"Course plugin for {course_name} not found.")
+        return None
+
+    course_class = getattr(course_module, course_name, None)
+    if course_class is None:
+        print(f"Course class for {course_name} not found in the module.")
+        return None
+
+    course = course_class()
+    return course.check_availability()
+
+def main():
+    course_names = COURSE_TO_DETECT
+    available_courses = []
+    for course_name in course_names:
+        result = check_course_availability(course_name)
+        if result:
+            available_courses.append((course_name, result))
+
+    if available_courses:
+        print("\nAvailable courses:")
+        for course_name, message in available_courses:
+            print(f"{course_name}: {message}")
+    else:
+        print("\nNo courses are currently available.")
+        sys.exit(0)
+
+    # if any of the courses are available, exit with 1, to make the action fail, to trigger the email notification
+    sys.exit(1)
+
+if __name__ == "__main__":
+    main()
