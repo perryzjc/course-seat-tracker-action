5b5ea68745c0844167ba1fec62881b2bb7cbccad
Jingchao Zhong <92573736+perryzjc@users.noreply.github.com>
2024-01-05 22:28:39 -0800

    Update engin_183d.py

--- courses/engin_183d.py
+++ courses/engin_183d.py
@@ -20,7 +20,7 @@ class ENGIN_183D(BaseCourse):
         try:
             data = json.loads(data_json)
             enrolled = data.get('available', {}).get('enrollmentStatus', {}).get('enrolledCount', 0)
-            available = 60 - enrolled
+            available = enrolled > 20
             message = f"{enrolled} enrolled out of 60 spots"
             return available, message
         except json.JSONDecodeError as e:
