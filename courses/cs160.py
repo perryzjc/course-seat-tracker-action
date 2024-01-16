3d40ff807299b58836ef65bbeac96a8bfdfb849f
Jingchao Zhong <92573736+perryzjc@users.noreply.github.com>
2024-01-15 16:27:35 -0800

    Fix CS160 total open seats logic

--- courses/cs160.py
+++ courses/cs160.py
@@ -13,20 +13,49 @@ class CS160(BaseCourse):
 
     def parse_html(self, html):
         soup = BeautifulSoup(html, 'html.parser')
-        data_element = soup.find(class_="fspmedium")
+        data_element = soup.find(attrs={'data-enrollment': True})
         if data_element:
-            return self.extract_data(data_element)
+            return self.extract_data(data_element['data-enrollment'])
         else:
             print("Could not find the required element on the page.")
             sys.exit(1)
 
-    def extract_data(self, data):
+    def extract_data(self, data_json):
         try:
-            data = data.text
-            disc_open_seats = int(data.strip())
-            available = disc_open_seats > 0
-            message = f"CS 160 discussion has {disc_open_seats} open seat now!"
+            data = json.loads(data_json)
+            # enrolled = data.get('available', {}).get('enrollmentStatus', {}).get('enrolledCount', 0)
+            total_open_seats = self.calculate_total_open_seats(data.get('available', {}))
+            available = total_open_seats > 0
+            message = f"CS160 Discussion has {total_open_seats} seats opened!"
             return available, message
         except json.JSONDecodeError as e:
             print(f"Failed to parse JSON: {e}")
             sys.exit(1)
+
+    @staticmethod
+    def calculate_total_open_seats(available):
+        """Algorithm to calculate total open seats for UC Berkeley's courses
+
+        Note: it's not a simply max_enroll - enrolled_count
+        Below code refers to:
+        https://classes.berkeley.edu/sites/default/files/js/js_wkZa4u4BCnSi4JXgkE3Om2OjgDKSaG35ZwAKoHBOzqI.js
+
+        >>> available = {
+        ...        'combination': {
+        ...            'maxEnrollCombinedSections': 90,
+        ...            'enrolledCountCombinedSections': 118
+        ...        },
+        ...        'enrollmentStatus': {
+        ...            'maxEnroll': 74,
+        ...            'enrolledCount': 72
+        ...        }}
+        >>> CS160.calculate_total_open_seats(available)
+        0
+        """
+        if 'combination' in available:
+            combined_open_seats = available['combination']['maxEnrollCombinedSections'] - available['combination']['enrolledCountCombinedSections']
+            per_class_open_seats = available['enrollmentStatus']['maxEnroll'] - available['enrollmentStatus']['enrolledCount']
+            value = min(combined_open_seats, per_class_open_seats)
+            return max(value, 0)
+        else:
+            return max(available['enrollmentStatus']['maxEnroll'] - available['enrollmentStatus']['enrolledCount'], 0)
--- exp.py
+++ exp.py
@@ -1,43 +1,35 @@
-import requests
-from bs4 import BeautifulSoup
-import json
+def calculate_total_open_seats(available):
+    if 'combination' in available:
+        combined_open_seats = available['combination']['maxEnrollCombinedSections'] - available['combination']['enrolledCountCombinedSections']
+        per_class_open_seats = available['enrollmentStatus']['maxEnroll'] - available['enrollmentStatus']['enrolledCount']
+        value = min(combined_open_seats, per_class_open_seats)
+        return max(value, 0)
+    else:
+        return max(available['enrollmentStatus']['maxEnroll'] - available['enrollmentStatus']['enrolledCount'], 0)
 
+# Example usage 1 - CS160, discussion section
+# available = {
+#     'combination': {
+#         'maxEnrollCombinedSections': 90,
+#         'enrolledCountCombinedSections': 118
+#     },
+#     'enrollmentStatus': {
+#         'maxEnroll': 74,
+#         'enrolledCount': 72
+#     }
+# }
 
-headers = {
- 'content-length':'39',
- 'sec-ch-ua':'"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
- 'sec-ch-ua-mobile':'?0',
- '__isguestuser':'true',
- 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
- 'content-type':'application/json, charset=UTF-8',
- 'accept':'application/json, text/javascript, */*; q=0.01',
- 'x-requested-with':'XMLHttpRequest',
- '__requestverificationtoken':'1X7gOIlyNSWCXEGBE5mlzIi9VMAyRYJ4PmUy-RfMxDkSry4uMFP7PYtUCyHcd7Z6jP9AULMMuIOALeWdjziddGeENtzB1zVXdbAToX3E2jc1',
- 'origin':'https://selfservice.elcamino.edu',
- 'sec-fetch-site':'same-origin',
- 'sec-fetch-mode':'cors',
- 'sec-fetch-dest':'empty',
- 'referer':'https://selfservice.elcamino.edu/Student/Courses/Search?keyword=CSCI-8',
- 'cookie':'__RequestVerificationToken_L1N0dWRlbnQ1=eu4xFpxLZkqreQcsa9aD8A1YPNOXhI9VoiJPW504BJI9NsL321zT5wOG-AgZJr5xXNBx4fZzx4bWTxsZ7QEryvZNuElfgWF8APmmU_AGTyA1'
+# Example usage 2 - CS160, Lecture
+available = {
+    'combination': {
+        'maxEnrollCombinedSections': 120,
+        'enrolledCountCombinedSections': 118
+    },
+    'enrollmentStatus': {
+        'maxEnroll': 74,
+        'enrolledCount': 72
+    }
 }
-payload=base64.b64decode("eyJzZWN0aW9uSWQiOiIyNDczODUiLCJzdHVkZW50SWQiOm51bGx9")
 
-
-def fetch_waitlist_seat():
-    try:
-        response = requests.request("POST", "https://selfservice.elcamino.edu/Student/Courses/SectionDetails", headers=headers, data=payload)
-        # Parse the JSON response
-        data = json.loads(response.text)
-
-        # Extract the AvailabilityDisplay field and split to get the waitlist number
-        availability_display = data.get("AvailabilityDisplay", "")
-        waitlist_number = availability_display.split('/')[-1].strip()
-
-        return waitlist_number
-    
-    except Exception as e:
-        return f"Error: {e}"
-    
-# Fetch the waitlist seat
-waitlist_seat = fetch_waitlist_seat()
-print("Waitlist Number:", waitlist_seat)
+total_open_seats = calculate_total_open_seats(available)
+print("Total Open Seats:", total_open_seats)
